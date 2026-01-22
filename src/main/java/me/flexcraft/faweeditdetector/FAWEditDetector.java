78085b9e85276bf38f55d52776c19c28e331b50d
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-22 03:54:23 +0100

    Update FAWEditDetector.java

--- src/main/java/me/flexcraft/faweeditdetector/FAWEditDetector.java
+++ src/main/java/me/flexcraft/faweeditdetector/FAWEditDetector.java
@@ -2,10 +2,20 @@ package me.flexcraft.faweeditdetector;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class FAWEditDetector extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
+        // создаём папку logs
+        File logsDir = new File(getDataFolder(), "logs");
+        if (!logsDir.exists()) {
+            logsDir.mkdirs();
+        }
+
         getLogger().info("FAWEditDetector enabled");
     }
 
