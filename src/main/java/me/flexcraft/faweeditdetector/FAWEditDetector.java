1203d221f59ed3c1c52599a69a03f6f5c5857cdd
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-22 04:47:04 +0100

    Update FAWEditDetector.java

--- src/main/java/me/flexcraft/faweeditdetector/FAWEditDetector.java
+++ src/main/java/me/flexcraft/faweeditdetector/FAWEditDetector.java
@@ -1,35 +1,48 @@
 package me.flexcraft.faweeditdetector;
 
+import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FAWEditDetector extends JavaPlugin {
 
-    private MySQLManager mySQLManager;
+    private SQLiteManager database;
+    private LuckPerms luckPerms;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
 
-        getLogger().info("Запуск FAWEditDetector...");
+        luckPerms = Bukkit.getServicesManager()
+                .getRegistration(LuckPerms.class).getProvider();
 
-        mySQLManager = new MySQLManager(this);
-
-        if (!mySQLManager.connect()) {
-            getLogger().severe("MySQL НЕ ПОДКЛЮЧЕНА! Плагин отключён.");
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
-        }
+        database = new SQLiteManager(this);
+        database.connect();
+        database.createTable();
 
         Bukkit.getPluginManager().registerEvents(
-                new FaweListener(this),
-                this
+                new FaweListener(this, luckPerms), this
         );
 
-        getLogger().info("FAWEditDetector успешно запущен.");
+        startCleanupTask();
+        getLogger().info("FAWEditDetector запущен (SQLite)");
     }
 
-    public MySQLManager getMySQLManager() {
-        return mySQLManager;
+    private void startCleanupTask() {
+        if (!getConfig().getBoolean("cleanup.enabled")) return;
+
+        int minutes = getConfig().getInt("cleanup.interval-minutes");
+        int days = getConfig().getInt("cleanup.days");
+
+        Bukkit.getScheduler().runTaskTimerAsynchronously(
+                this,
+                () -> database.cleanupOldLogs(days),
+                20L,
+                minutes * 60L * 20L
+        );
+    }
+
+    public SQLiteManager getDatabase() {
+        return database;
     }
 }
