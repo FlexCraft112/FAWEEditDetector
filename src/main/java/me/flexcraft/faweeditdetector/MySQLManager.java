982d48f6fca4838e5213f88e88b312dd0448a3f4
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-22 04:14:36 +0100

    Create MySQLManager.java

---
+++ src/main/java/me/flexcraft/faweeditdetector/MySQLManager.java
@@ -0,0 +1,80 @@
+package me.flexcraft.faweeditdetector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class MySQLManager {
+
+    private Connection connection;
+    private final FAWEditDetector plugin;
+
+    public MySQLManager(FAWEditDetector plugin) {
+        this.plugin = plugin;
+    }
+
+    /**
+     * Подключение к MySQL
+     */
+    public boolean connect() {
+        try {
+            String host = plugin.getConfig().getString("mysql.host");
+            int port = plugin.getConfig().getInt("mysql.port");
+            String database = plugin.getConfig().getString("mysql.database");
+            String user = plugin.getConfig().getString("mysql.user");
+            String password = plugin.getConfig().getString("mysql.password");
+            boolean ssl = plugin.getConfig().getBoolean("mysql.useSSL");
+
+            String url =
+                    "jdbc:mysql://" + host + ":" + port + "/" + database +
+                    "?useSSL=" + ssl +
+                    "&useUnicode=true" +
+                    "&characterEncoding=utf8" +
+                    "&autoReconnect=true";
+
+            connection = DriverManager.getConnection(url, user, password);
+
+            createTable();
+
+            plugin.getLogger().info("MySQL подключена успешно");
+            return true;
+
+        } catch (Exception e) {
+            plugin.getLogger().severe("Ошибка подключения к MySQL");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Создание таблицы, если её нет
+     */
+    private void createTable() throws Exception {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(
+                "CREATE TABLE IF NOT EXISTS fawe_edits (" +
+                        "id INT AUTO_INCREMENT PRIMARY KEY," +
+                        "player VARCHAR(32) NOT NULL," +
+                        "uuid VARCHAR(36) NOT NULL," +
+                        "group_name VARCHAR(32) NOT NULL," +
+                        "world VARCHAR(64) NOT NULL," +
+                        "blocks BIGINT NOT NULL," +
+                        "min_x INT NOT NULL," +
+                        "min_y INT NOT NULL," +
+                        "min_z INT NOT NULL," +
+                        "max_x INT NOT NULL," +
+                        "max_y INT NOT NULL," +
+                        "max_z INT NOT NULL," +
+                        "time TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                        ")"
+        );
+        statement.close();
+    }
+
+    /**
+     * Получить активное соединение
+     */
+    public Connection getConnection() {
+        return connection;
+    }
+}
